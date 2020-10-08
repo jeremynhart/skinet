f5302d0d47a0816ede8a2f910a1a8c6ef5d6315d
Jeremy Hart <jeremynhart@gmail.com>
2020-10-08 05:14:51 -0600

    Infrastructure.cs created, ProductRepository.cs, IProductRepository.cs created & implemented

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductIdAsync(int id);
+
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+    }
+}
